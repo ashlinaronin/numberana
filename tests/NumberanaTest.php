cb3fdaf8ea2c23a3dec82a33f4ebdd1c959a9375
Ashlin Aronin & Sam Martinez <student@epicodus.com>
2015-08-13 15:37:14 -0700

    add test for three digit number and fail it

--- tests/NumberanaTest.php
+++ tests/NumberanaTest.php
@@ -43,6 +43,16 @@
 
             $this->assertEquals("twenty-two", $result);
         }
+
+        function test_numberToWord_threeDigitNumber()
+        {
+            $test_Numberana = new Numberana;
+            $input_number = 100;
+
+            $result = $test_Numberana->numberToWord($input_number);
+
+            $this->assertEquals("one-hundred", $result);
+        }
     }
 
  ?>
